fccb6672939999270c81fcbd2849373d3ab7f8a2
Oskaras <66420051+Nzzska@users.noreply.github.com>
2022-10-11 23:51:08 +0300

    pridetas testas ir pan.

--- cust-mobjects/attached_vector.py
+++ cust-mobjects/attached_vector.py
@@ -1,5 +1,3 @@
-from turtle import position
-from unicodedata import name
 from manim import *
 
 class ObjectDescription:
@@ -7,7 +5,8 @@ class ObjectDescription:
         self, 
         mobject: Mobject, 
         name: str,
-        direction: np.ndarray
+        direction: np.ndarray,
+        **kwargs
     ):
         self._mobject = mobject
         self._name = name
@@ -15,19 +14,42 @@ class ObjectDescription:
         self._direction = direction
 
     @property
-    def direction(self) -> Arrow:
-        pass
+    def direction(self) -> np.ndarray:
+        return self._direction
+
+    @direction.setter
+    def direction(self, new_direction: np.ndarray):
+        self._direction = new_direction
 
     @property
     def position(self) -> np.ndarray:
-        pass
+        return self._position
+
+    @position.setter
+    def position(self, new_position: np.ndarray):
+        self._position = new_position
 
     @property
     def name(self) -> str:
-        pass
+        return self._name
+    
+    @name.setter
+    def name(self, new_name: str):
+        self._name = new_name
 
-    def create_vector(self) -> Arrow:
-        pass
+    ########
+    @property
+    def vector(self, **kwargs):
+        return self.create_vector(**kwargs)
+    ########
+
+    def create_vector(self, **kwargs) -> Arrow:
+        return Arrow(
+            start=self._position,
+            end=self._position + self._direction,
+            buff=0,
+            **kwargs
+        )
 
     def __str__(self) -> str:
         return self.name
